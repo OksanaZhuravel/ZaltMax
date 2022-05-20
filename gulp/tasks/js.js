70b674678012c1c8086c4e4e3aed4f2f53adbe0c
OksanaZhuravel <opuseva@mail.ru>
2022-05-20 20:49:42 +0400

    new block added

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,24 +1,76 @@
 import webpack from 'webpack-stream';
 
 export const js = () => {
-  return app.gulp
-    .src(app.path.src.js, { sourcemaps: app.isDev })
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: 'JS',
-          message: 'Error: <%= error.message %>',
+  return (
+    app.gulp
+      .src(app.path.src.js, { sourcemaps: app.isDev })
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: 'JS',
+            message: 'Error: <%= error.message %>',
+          })
+        )
+      )
+      .pipe(
+        webpack({
+          mode: app.isBuild ? 'production' : 'development',
+          output: {
+            filename: 'app.min.js',
+          },
+          module: {
+            rules: [
+              {
+                test: /\.(scss|css)$/,
+                use: [
+                  'style-loader',
+                  {
+                    loader: 'string-replace-loader',
+                    options: {
+                      search: '@img',
+                      replace: '../img',
+                      flags: 'g',
+                    },
+                  },
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      importLoaders: 1,
+                      sourceMap: true,
+                      modules: false,
+                      url: {
+                        filter: (url, resourcePath) => {
+                          if (url.includes('img') || url.includes('fonts')) {
+                            return false;
+                          }
+                          return true;
+                        },
+                      },
+                    },
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sassOptions: {
+                        outputStyle: 'expanded',
+                      },
+                    },
+                  },
+                ],
+              },
+            ],
+          },
         })
       )
-    )
-    .pipe(
-      webpack({
-        mode: app.isBuild ? 'production' : 'development',
-        output: {
-          filename: 'app.min.js',
-        },
-      })
-    )
-    .pipe(app.gulp.dest(app.path.build.js))
-    .pipe(app.plugins.browsersync.stream());
+      // .pipe(
+      //   webpack({
+      //     mode: app.isBuild ? 'production' : 'development',
+      //     output: {
+      //       filename: 'app.min.js',
+      //     },
+      //   })
+      // )
+      .pipe(app.gulp.dest(app.path.build.js))
+      .pipe(app.plugins.browsersync.stream())
+  );
 };
